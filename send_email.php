6c508a9912a0b7f3cf4d593a5df4c917252288f2
Yohannes Mariam <yohannesjrmariam@gmail.com>
2024-07-23 13:07:40 -0400

    Intial commit

---
+++ send_email.php
@@ -0,0 +1,42 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form data
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST["subject"]));
+    $message = trim($_POST["message"]);
+
+    // Check if any field is empty
+    if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+        http_response_code(400);
+        echo "Please fill out all fields.";
+        exit;
+    }
+
+    // Set the recipient email address
+    $recipient = "yohannesjrmariam@gmail.com";
+
+    // Set the email subject
+    $email_subject = "New contact form submission: $subject";
+
+    // Build the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers
+    $email_headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($recipient, $email_subject, $email_content, $email_headers)) {
+        http_response_code(200);
+        echo "Thank you! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong, and we couldn't send your message.";
+    }
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission. Please try again.";
+}
+?>
